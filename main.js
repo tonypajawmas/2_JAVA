bb385e9f2f07b78290922429ee6a0ff788b10145
tonypajawmas <91402942+tonypajawmas@users.noreply.github.com>
2021-11-28 17:24:17 -0800

    first

---
+++ main.js
@@ -0,0 +1,22 @@
+// declare a variable using the keyword "let" and the identifier aka the name of "cats" and assign the value of 22
+let cats = 22;
+
+// declare a variable using the identifier aka the name of "dogs" and assign the value of 13
+let dogs = 13;
+
+// declare a variable using the identifier aka the name of "addingVariables" and assign the expression of "cats + dogs"
+let addingVariables = cats + dogs;
+
+// declare a variable using the identifier aka the name of "addingStrings" and concat 'javascript is ' + 'really awesome!'
+let aPhrase = "javascript is ";
+let bPhrase = "really awesome!";
+let addingStrings = aPhrase + bPhrase;
+
+// declare an array using the identifier aka the name of "myArray" and add these three items into it: 'Coding is awesome', 500, false
+let myArray = ["Coding is awesome", 500, false];
+
+// resassign myArray[1] to 250
+myArray[1] = 250;
+
+// creat your own variable and write a random string/message in it
+let tony = "good coder";
